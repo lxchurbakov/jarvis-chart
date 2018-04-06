f8b66d14a177c7501cb6d68a5aa10e24d9fbd9fd
mcfinley <mcfinleyandrew@gmail.com>
2018-04-06 12:06:42 +0300

    Add index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('');
+import "./index.html";
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import Chart from "./Chart";
+
+const node = document.getElementById("root");
+
+ReactDOM.render(<Chart />, node);
