2f542524a3a5140f092a1fa3a221e80c26792a2b
mcfinley <mcfinleyandrew@gmail.com>
2018-04-06 10:28:31 +0300

    Create simple matrix lib

---
+++ src/matrix/index.js
@@ -0,0 +1,98 @@
+/**
+ * Matrix
+ *
+ * [ a,  b, 0]
+ * [ c,  d, 0]
+ * [tx, ty, 1]
+ *
+ *
+ */
+class Matrix {
+  constructor (a, b, c, d, tx, ty) {
+    this.matrix = [
+      [ a,  b, 0],
+      [ c,  d, 0],
+      [tx, ty, 1],
+    ];
+  }
+
+  get (x, y) {
+    return this.matrix[y][x];
+  }
+
+  toCss () {
+    const [
+      [ a,  b, _0],
+      [ c,  d, _1],
+      [tx, ty, _2]
+    ] = this.matrix;
+
+    return `matrix(${a}, ${b}, ${c}, ${d}, ${tx}, ${ty})`;
+  }
+}
+
+Matrix.nope = (x, y) => {
+  const a = 1;
+  const b = 0;
+  const c = 0;
+  const d = 1;
+  const tx = 0;
+  const ty = 0;
+
+  return new Matrix(a, b, c, d, tx, ty);
+};
+
+Matrix.scale = (x, y) => {
+  const a = x;
+  const b = 0;
+  const c = 0;
+  const d = y;
+  const tx = 0;
+  const ty = 0;
+
+  return new Matrix(a, b, c, d, tx, ty);
+};
+
+Matrix.translate = (x, y) => {
+  const a = 1;
+  const b = 0;
+  const c = 0;
+  const d = 1;
+  const tx = x;
+  const ty = y;
+
+  return new Matrix(a, b, c, d, tx, ty);
+};
+
+Matrix.rotate = (alpha) => {
+  const cos = Math.cos(alpha)
+  const sin = Math.sin(alpha);
+
+  const a = cos;
+  const b = sin;
+  const c = -sin;
+  const d = cos;
+  const tx = 0;
+  const ty = 0;
+
+  return new Matrix(a, b, c, d, tx, ty);
+};
+
+const matrixMultiplicationStep = (A, B, x, y) =>
+  A.get(x, 0) * B.get(0, y) + A.get(x, 1) * B.get(1, y) + A.get(x, 2) * B.get(2, y);
+
+Matrix.mul = (A, B) => {
+  const a = matrixMultiplicationStep(A, B, 0, 0);
+  const b = matrixMultiplicationStep(A, B, 1, 0);
+  const c = matrixMultiplicationStep(A, B, 0, 1);
+  const d = matrixMultiplicationStep(A, B, 1, 1);
+
+  const tx = matrixMultiplicationStep(A, B, 0, 2);
+  const ty = matrixMultiplicationStep(A, B, 1, 2);
+
+  return new Matrix(a, b, c, d, tx, ty);
+};
+
+Matrix.toRad = (alpha) => (alpha / 360) * Math.PI * 2;
+
+export default Matrix;
