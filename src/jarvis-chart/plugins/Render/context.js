861326a64d6c724d9aa9739060752ce32333ba3c
mcfinley <mcfinleyandrew@gmail.com>
2018-04-10 14:17:08 +0300

    Add debug plugin and implement double buffering

--- src/jarvis-chart/plugins/Render/context.js
+++ src/jarvis-chart/plugins/Render/context.js
@@ -48,19 +48,30 @@ const buildCanvasContext = (node, options) => {
   const height = node.offsetHeight;
 
   const canvas = document.createElement('canvas');
+  const buffer = document.createElement('canvas');
 
   canvas.width = width;
   canvas.height = height;
+
+  buffer.width = width;
+  buffer.height = height;
 
   options.width = width;
   options.height = height;
 
   node.appendChild(canvas);
 
-  const context = canvas.getContext('2d');
+  const realContext = canvas.getContext('2d');
+  const context = buffer.getContext('2d');
+  // const context = canvas.getContext('2d');
+  // const buffer  = document.createElement('canvas').getContext('2d');
 
-  context.clear = () => context.clearRect(0, 0, width, height);
+  context.clear = () => {
+    context.clearRect(0, 0, width, height);
+    realContext.clearRect(0, 0, width, height);
+  };
   context.flush = () => { /* nope */ };
+  context.flush = () => realContext.drawImage(buffer, 0, 0);
 
   /* Attach matrix API */
   context.matrix = Transform({
