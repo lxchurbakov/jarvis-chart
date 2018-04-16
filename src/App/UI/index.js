ea9f7c5daddafa0379de952fe03de5b970c259d5
mcfinley <mcfinleyandrew@gmail.com>
2018-04-16 12:14:29 +0300

    Implement left panel

--- src/App/UI/index.js
+++ src/App/UI/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { ArrowLeftIcon, ArrowRightIcon, ZoomInIcon, ZoomOutIcon, CalendarIcon, UndoIcon } from './icons';
+import {
+  ArrowLeftIcon, ArrowRightIcon, ZoomInIcon, ZoomOutIcon, CalendarIcon, UndoIcon,
+  FileIcon, EyeIcon, PadlockIcon, PaintIcon, VectorIcon, LineCircleIcon, LineCircleHorizontalIcon, SegmentDiagonalIcon, SegmentHorizontalIcon,
+  RectangleIcon, CircleIcon, TriangleIcon, GoldenRatioIcon, EraserIcon, PencilIcon, TextIcon, RedoIcon
+} from './icons';
 import CircleButton from './CircleButton';
 
 const UIWrap = styled.div`
@@ -11,6 +15,11 @@ const UIWrap = styled.div`
       opacity: 1;
       pointer-events: auto;
       bottom: 20px;
+    }
+    *[data-ui=left] {
+      opacity: 1;
+      pointer-events: auto;
+      left: 20px;
     }
   }
 `;
@@ -22,7 +31,8 @@ const BottomUIWrap = styled.div.attrs({ 'data-ui': 'bottom' })`
   position: absolute;
   bottom: 0;
   left: 50%;
-  transform: translate(-50%);
+  transform: translate(-50%, 0);
+  display: flex;
   & > * {
     margin: 5px;
   }
@@ -36,9 +46,9 @@ const BottomUI = ({ onLeft, onRight, onZoomIn, onZoomOut }) => (
     <CircleButton onClick={onZoomIn}>
       <ZoomInIcon width={25} height={20} />
     </CircleButton>
-    <CircleButton>
+    {/* <CircleButton>
       <UndoIcon width={25} height={20} />
-    </CircleButton>
+    </CircleButton> */}
     <CircleButton onClick={onZoomOut}>
       <ZoomOutIcon width={25} height={20} />
     </CircleButton>
@@ -48,9 +58,69 @@ const BottomUI = ({ onLeft, onRight, onZoomIn, onZoomOut }) => (
   </BottomUIWrap>
 );
 
-export default ({ children, onLeft, onRight, onZoomIn, onZoomOut }) => (
+const LeftUIWrap = styled.div.attrs({ 'data-ui': 'left' })`
+  pointer-events: none;
+  opacity: 0;
+  transition: all 200ms ease;
+  position: absolute;
+  left: 0;
+  top: 50%;
+  transform: translate(0, -50%);
+  display: flex;
+  flex-direction: column;
+  & > * {
+    margin: 5px;
+    cursor: pointer;
+    transition: transform 200ms ease;
+    &:hover {
+      transform: scale(1.2);
+      transform-origin: 50% 50%;
+    }
+  }
+`;
+
+const LeftUI = ({
+  onEye, onPaint,
+  onVector, onLineCircle, onLineCircleHorizontal, onSegmentDiagonal,
+  onSegmentHorizontal, onRectangle, onCircle, onTriangle, onText,
+}) => (
+  <LeftUIWrap>
+    {/* <div><FileIcon width={25} height={25} /></div> */}
+    <div onClick={onEye}><EyeIcon width={25} height={25} /></div>
+    {/* <div><PadlockIcon width={25} height={25} /></div> */}
+    <div onClick={onPaint}><PaintIcon width={25} height={25} /></div>
+    <div onClick={onVector}><VectorIcon width={25} height={25} /></div>
+    <div onClick={onLineCircle}><LineCircleIcon width={25} height={25} /></div>
+    <div onClick={onLineCircleHorizontal}><LineCircleHorizontalIcon width={25} height={25} /></div>
+    <div onClick={onSegmentDiagonal}><SegmentDiagonalIcon width={25} height={25} /></div>
+    <div onClick={onSegmentHorizontal}><SegmentHorizontalIcon width={25} height={25} /></div>
+    <div onClick={onRectangle}><RectangleIcon width={25} height={25} /></div>
+    <div onClick={onCircle}><CircleIcon width={25} height={25} /></div>
+    <div onClick={onTriangle}><TriangleIcon width={25} height={25} /></div>
+    {/* <div onClick={onFile}><GoldenRatioIcon width={25} height={25} /></div> */}
+    {/* <div onClick={onFile}><EraserIcon width={25} height={25} /></div> */}
+    {/* <div onClick={onFile}><PencilIcon width={25} height={25} /></div> */}
+    <div onClick={onText}><TextIcon width={25} height={25} /></div>
+    {/* <div onClick={onFile}><UndoIcon width={25} height={25} /></div> */}
+    {/* <div onClick={onFile}><RedoIcon width={25} height={25} /></div> */}
+  </LeftUIWrap>
+);
+
+export default ({
+  children,
+  onPaint, onEye, onVector, onLineCircle, onLineCircleHorizontal, onSegmentDiagonal,
+  onSegmentHorizontal, onRectangle, onCircle, onTriangle, onText,
+  onLeft, onRight, onZoomIn, onZoomOut,
+}) => (
   <UIWrap>
     {children}
     <BottomUI onLeft={onLeft} onRight={onRight} onZoomIn={onZoomIn} onZoomOut={onZoomOut} />
+    <LeftUI
+      onPaint={onPaint} onEye={onEye} onVector={onVector}
+      onLineCircle={onLineCircle} onLineCircleHorizontal={onLineCircleHorizontal}
+      onSegmentDiagonal={onSegmentDiagonal} onSegmentHorizontal={onSegmentHorizontal}
+      onRectangle={onRectangle} onCircle={onCircle} onTriangle={onTriangle}
+      onText={onText}
+    />
   </UIWrap>
 );
--- src/App/index.js
+++ src/App/index.js
@@ -12,25 +12,28 @@ const doubleBuffer = (getParameterByName('double-buffer') || 'on') === 'on';
 const clickThreshold = 100;
 
 class App extends React.Component {
-  left = () => {
-    this.chart.chartWindowsScaleTranslate.translate(200);
-  }
+  left = () => this.chart.chartWindowsScaleTranslate.translate(200);
+  right = () => this.chart.chartWindowsScaleTranslate.translate(-200);
 
-  right = () => {
-    this.chart.chartWindowsScaleTranslate.translate(-200);
-  }
+  zoomIn = () => this.chart.chartWindowsScaleTranslate.zoom(1);
+  zoomOut = () => this.chart.chartWindowsScaleTranslate.zoom(-1);
 
-  zoomIn = () => {
-    this.chart.chartWindowsScaleTranslate.zoom(1);
-  }
+  paint = () => this.chart.chartWindowsModes.set('brush');
+  eye = () => this.chart.chartWindowsModes.set('view');
+  vector = () => this.chart.chartWindowsModes.set('vector');
 
-  zoomOut = () => {
-    this.chart.chartWindowsScaleTranslate.zoom(-1);
-  }
+  lineCircle = () => this.chart.chartWindowsModes.set('line');
+  lineCircleHorizontal = () => this.chart.chartWindowsModes.set('line-horizontal');
+  segmentDiagonal = () => this.chart.chartWindowsModes.set('segment');
+  segmentHorizontal = () => this.chart.chartWindowsModes.set('segment-horizontal');
+  rectangle = () => this.chart.chartWindowsModes.set('rectangle');
+  circle = () => this.chart.chartWindowsModes.set('circle');
+  triangle = () => this.chart.chartWindowsModes.set('triangle');
+  text = () => this.chart.chartWindowsModes.set('text');
 
   render () {
     const width = '100%';
-    const height = '600px';
+    const height = '800px';
 
     const options = {
       render, values, doubleBuffer, clickThreshold,
@@ -40,7 +43,23 @@ class App extends React.Component {
     return (
       <div>
         <h1>Jarvis Chart Example</h1>
-        <UI onLeft={this.left} onRight={this.right} onZoomIn={this.zoomIn} onZoomOut={this.zoomOut}>
+        <UI
+          onLeft={this.left}
+          onRight={this.right}
+          onZoomIn={this.zoomIn}
+          onZoomOut={this.zoomOut}
+          onPaint={this.paint}
+          onEye={this.eye}
+          onVector={this.vector}
+          onLineCircle={this.lineCircle}
+          onLineCircleHorizontal={this.lineCircleHorizontal}
+          onSegmentDiagonal={this.segmentDiagonal}
+          onSegmentHorizontal={this.segmentHorizontal}
+          onRectangle={this.rectangle}
+          onCircle={this.circle}
+          onTriangle={this.triangle}
+          onText={this.text}
+        >
           <Chart ref={n => this.chart = n.chart} options={options} />
         </UI>
       </div>
