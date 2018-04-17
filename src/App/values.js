4c6255c7bb0d6a2e7ea97511c3b44b26995036fe
mcfinley <mcfinleyandrew@gmail.com>
2018-04-17 13:37:08 +0300

    Improve values generation a bit

--- src/App/values.js
+++ src/App/values.js
@@ -1,4 +1,4 @@
-const VALUES_SAMPLE_COUNT = 1000;
+const VALUES_SAMPLE_COUNT = 10000;
 
 /* Shortcut for the value */
 const value = (min, max, open, close, time, volume) => ({ min, max, open, close, time, volume });
@@ -8,7 +8,7 @@ const toTwo = (v) => (new Array(2 - v.length).fill('0').join('')) + v;
 
 /* Get time from index */
 const getTime = (index) => {
-  const hours   = Math.floor(index / 60);
+  const hours   = Math.floor(index / 60) % 24;
   const minutes = (index % 60);
 
   return `${toTwo(hours.toString())}:${toTwo(minutes.toString())}`;
@@ -16,6 +16,7 @@ const getTime = (index) => {
 
 /* Values array */
 const values = [];
+const offset = Math.floor(Math.random() * VALUES_SAMPLE_COUNT)
 
 for (let i = 0; i < VALUES_SAMPLE_COUNT; ++i) {
   const open = i > 0 ? values[i - 1].close : Math.random() * 200 + 30;
@@ -24,7 +25,7 @@ for (let i = 0; i < VALUES_SAMPLE_COUNT; ++i) {
   const min = Math.min(open, close) - Math.random() * 20;
   const max = Math.max(open, close) + Math.random() * 50;
 
-  const time = getTime(i);
+  const time = getTime(i + offset);
 
   const volume = (max - min) * (Math.random() + 1)
 
